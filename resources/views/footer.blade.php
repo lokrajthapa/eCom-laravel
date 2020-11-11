998ae6880ea8116f9f44ce6a2d3654bf5a6f0bd5
lokrajthapa <lokrajthapa741@gmail.com>
2020-11-10 19:07:11 -0800

    second time push

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,1 +1,36 @@
-<h1>  this is footer </h1>
+<!-- Footer -->
+<footer class="page-footer font-small blue pt-4">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">Footer Content</h5>
+        <p>Here you can use rows and columns to organize your footer content.</p>
+
+      </div>
+      <!-- Grid column -->
+
+     
+      <!-- Grid<div class="col-md-3 mb-md-0 mb-3"> column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
